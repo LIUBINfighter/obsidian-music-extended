a2341fbde8da96729b7a9785ee6496336c78916b
JayBridge <12310903@mail.sustech.edu.cn>
2025-04-03 08:49:03 +0800

    更新 README，添加 metadata-utils.ts 文件，修改音乐库视图激活方式，支持在新标签页或工作区打开

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## 初步开发规范
 
 utils.ts obsidian原生接口
+
+metadata-utils.ts music-metadata相关工具函数
--- src/main.ts
+++ src/main.ts
@@ -21,25 +21,33 @@ export default class MusicExtendedPlugin extends Plugin {
 			(leaf: WorkspaceLeaf) => new GalleryView(leaf)
 		);
 		
-		// 添加ribbon图标
+		// 添加ribbon图标 - 默认在新标签页打开
 		const ribbonIconEl = this.addRibbonIcon(
-			'music-note', // 使用内置的音乐图标
+			'file-music', // 使用内置的音乐图标
 			'打开音乐库', // 悬停提示文本
 			(evt: MouseEvent) => {
-				// 点击时激活音乐画廊视图
-				this.activateView();
+				// 点击时在新标签页中激活音乐画廊视图
+				this.activateView('tab');
 			}
 		);
 		
 		// 给ribbon图标元素添加一个CSS类，方便样式定制
 		ribbonIconEl.addClass('music-extended-ribbon-icon');
 		
-		// 添加命令
+		// 添加多个命令
 		this.addCommand({
-			id: 'open-music-gallery',
-			name: '打开音乐库',
+			id: 'open-music-gallery-new-tab',
+			name: '在新标签页打开音乐库',
 			callback: () => {
-				this.activateView();
+				this.activateView('tab');
+			},
+		});
+		
+		this.addCommand({
+			id: 'open-music-gallery-workspace',
+			name: '在工作区打开音乐库',
+			callback: () => {
+				this.activateView('workspace');
 			},
 		});
 
@@ -60,23 +68,45 @@ export default class MusicExtendedPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 	
-	async activateView() {
-		// 激活视图，如果不存在就创建新的
+	/**
+	 * 激活音乐库视图
+	 * @param mode - 激活模式: 'tab'表示新建标签页, 'workspace'表示在工作区打开
+	 */
+	async activateView(mode: 'tab' | 'workspace' = 'tab') {
 		const { workspace } = this.app;
 		
-		let leaf = workspace.getLeavesOfType(GALLERY_VIEW_TYPE)[0];
-		
-		if (!leaf) {
-			// 创建新的页签
-			leaf = workspace.getLeaf(false);
-			await leaf.setViewState({
-				type: GALLERY_VIEW_TYPE,
-				active: true,
-			});
+		if (mode === 'workspace') {
+			// 方式1: 在工作区中查找已有视图或创建
+			let leaf = workspace.getLeavesOfType(GALLERY_VIEW_TYPE)[0];
+			
+			if (!leaf) {
+				// 使用已存在的视图位置
+				leaf = workspace.getLeaf();
+				await leaf.setViewState({
+					type: GALLERY_VIEW_TYPE,
+					active: true,
+				});
+			}
+			
+			workspace.revealLeaf(leaf);
+		} else {
+			// 方式2: 创建新标签页(不覆盖当前视图)
+			// 首先查看是否已经有此类型的视图
+			const existingLeaf = workspace.getLeavesOfType(GALLERY_VIEW_TYPE)[0];
+			
+			if (existingLeaf) {
+				// 如果存在则激活已有视图
+				workspace.revealLeaf(existingLeaf);
+			} else {
+				// 创建新标签页
+				const leaf = workspace.getLeaf('tab');
+				await leaf.setViewState({
+					type: GALLERY_VIEW_TYPE,
+					active: true,
+				});
+				workspace.revealLeaf(leaf);
+			}
 		}
-		
-		// 聚焦到视图
-		workspace.revealLeaf(leaf);
 	}
 }
 
